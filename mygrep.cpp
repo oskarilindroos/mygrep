1831871d58c33e69e77d03feb098341b3141d968
Oskari Lindroos <oskari.a.lindroos@tuni.fi>
2022-02-12 15:27:55 +0200

    add option for command line arguments

--- mygrep.cpp
+++ mygrep.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
-int grep(string, string);
+void basicSearch();
+void fileSearch();
 
-int main()
+int main(int argc, char* argv[])
+{
+    switch (argc)
+    {
+        case 1:
+            basicSearch();
+            break;
+        case 2:
+            fileSearch();
+            break;
+    }
+
+    return 0;
+}
+
+void basicSearch()
 {
     string stringToSearch;
     string searchString;
-
-    int searchResult;
+    int found;
 
     cout << "Give a string from which to search for: ";
     getline(cin, stringToSearch);
@@ -18,19 +34,15 @@ int main()
     cout << "Give a search string: ";
     getline(cin, searchString);
 
-    searchResult = grep(stringToSearch, searchString);
-
-    if (searchResult != string::npos) {
-        cout << endl << "\""<< searchString << "\" found in \"" << stringToSearch << "\" in position " << searchResult;
-    } else {
-        cout << endl << searchString << " NOT found in " << stringToSearch;
-    }
-
-    return 0;
+    found = stringToSearch.find(searchString);
+    
+    if (found != string::npos)
+        cout << endl << "\"" << searchString << "\" found in \"" << stringToSearch << "\" in position " << found << endl;
+    else
+        cout << endl << "\"" << searchString << "\" NOT found in \"" << stringToSearch << "\"" << endl;
 }
 
-
-int grep(string stringToSearch, string searchString)
+void fileSearch()
 {
-    return stringToSearch.find(searchString);
+    ;
 }
