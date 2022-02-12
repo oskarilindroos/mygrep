2625fb6cae4e7fb2c4023d968d8915a3033c5f48
Oskari Lindroos <oskari.a.lindroos@tuni.fi>
2022-02-12 13:46:05 +0200

    add simple substring search

--- mygrep.cpp
+++ mygrep.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
-#include <cstring>
+#include <string>
 
 using namespace std;
 
+int grep(string, string);
+
 int main()
 {
-    
+    string stringToSearch;
+    string searchString;
 
+    int searchResult;
+
+    cout << "Give a string from which to search for: ";
+    getline(cin, stringToSearch);
+
+    cout << "Give a search string: ";
+    getline(cin, searchString);
+
+    searchResult = grep(stringToSearch, searchString);
+
+    if (searchResult != string::npos) {
+        cout << endl << "\""<< searchString << "\" found in \"" << stringToSearch << "\" in position " << searchResult;
+    } else {
+        cout << endl << searchString << " NOT found in " << stringToSearch;
+    }
 
     return 0;
+}
+
+
+int grep(string stringToSearch, string searchString)
+{
+    return stringToSearch.find(searchString);
 }
